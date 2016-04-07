09d1c43cd329a485e5f480a9ff35b09a27986eaf
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-04-07 16:13:05 +0300

    add Matrix tests

---
+++ Task1.Tests/DiagonalMatrixTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Task1.Tests
+{
+    
+    [TestFixture]
+    public class DiagonalMatrixTests
+    {
+
+        private readonly int[][] diagonalMatrixInts =
+            {
+                new int[3] {1, 0, 0},
+                new int[3] {0, 5, 0},
+                new int[3] {0, 0, 9},
+            };
+
+        
+
+        [Test]
+        public void DiagonalMatrixInts_TwoDimensionalArrayInts_InitializedWithoutExceptionsAndEqualToSourceArray()
+        {
+            var diagonalMatrix = new DiagonalMatrix<int>(diagonalMatrixInts);
+
+            Assert.IsTrue(SquareMatrix<int>.CompareToJaggedArray(diagonalMatrix, diagonalMatrixInts));
+        }
+
+        [Test]
+        [ExpectedException("System.ArgumentException")]
+        public void DiagonalMatrixInts_TwoDimensionalArrayInts_InitializedWithException()
+        {
+            int[][] squareMatrix =
+            {
+                new int[3] {1, 4, 25},
+                new int[3] {12, 5, 1},
+                new int[3] {34, 0, 9},
+            };
+
+            var diagonalMatrix = new DiagonalMatrix<int>(squareMatrix);
+        }
+
+        [Test]
+        public void DiagonalMatrixInts_ChangeDiagonalElement_EventGenerated()
+        {
+            var diagonalMatrix = new DiagonalMatrix<int>(diagonalMatrixInts);
+            var subscriber = new MatrixTestSubscriber<int>(); 
+
+            subscriber.Subscribe(diagonalMatrix);
+            diagonalMatrix[2] = 99;
+
+            Assert.IsTrue(subscriber.WasCalled);
+        }
+
+    }
+}
---
+++ Task1.Tests/MatrixTestSubscriber.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace Task1.Tests
+{
+    public class MatrixTestSubscriber<T>
+    {
+        public bool WasCalled { get; private set; } = false;
+
+        public void Subscribe(SquareMatrix<T> matrix)
+        {
+            matrix.ElementChanged += EventGenerated;
+        }
+
+        private void EventGenerated(object sender, MatrixChangedEventArgs<T> args)
+        {
+            Debug.WriteLine($"Event generated\n row: {args.Row}, column: {args.Column}, new value: {args.newValue}");
+            WasCalled = true;
+        }      
+    }
+}
